ba8d1dc049333bd3ea76e9705de51ce436fa365f
wwei-github <418047394@qq.com>
2023-02-14 00:25:12 +0800

    style: change font-family

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 import { StyledEngineProvider } from "@mui/material/styles";
 import App from "./App";
@@ -13,13 +14,21 @@ const client = new ApolloClient({
   uri: watcherUri,
   cache: new InMemoryCache(),
 });
+const theme = createTheme({
+  typography: {
+    fontFamily: "Inter",
+    fontStyle: "normal",
+  },
+});
 
 const container = document.querySelector("#root");
 const root = ReactDOM.createRoot(container);
 root.render(
   <ApolloProvider client={client}>
     <StyledEngineProvider injectFirst>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </StyledEngineProvider>
   </ApolloProvider>
 );
