5797b497f83551d6df1309926a86ce16725cd026
Bruce Xu <bruce.xu@lxdao.io>
2023-02-20 16:41:33 +1300

    feat: finish footer ui polishing

--- src/views/FooterBox.jsx
+++ src/views/FooterBox.jsx
@@ -6,93 +6,88 @@ import lxdaoLogo from "../assets/lxdaoLogo.png";
 
 function FooterBox() {
   return (
-    <Box
-      backgroundColor="#F1F1F1"
-      height="557px"
-      paddingTop="88px"
-      paddingLeft="256px">
-      <Box display="flex" justifyContent="flex-start" alignItems="center">
-        <Typography
-          component="img"
-          src={mobymaskLogo}
-          width="71px"
-          marginRight="15px"
-          alt="logo"
-        />
-        <Typography component="span" fontSize="31px" fontWeight="600">
-          MobyMask
-        </Typography>
-      </Box>
-
-      <Box
-        display="flex"
-        justifyContent="flex-start"
-        flexWrap="wrap"
-        fontSize="18px"
-        fontWeight="600"
-        color="#101828"
-        marginTop="68px">
-        <Box display="flex" justifyContent="flex-start" width="456px">
-          <Box component="p" margin="0" marginRight="20px" flexShrink="0">
-            <Typography
-              component="a"
-              color="#000"
-              fontWeight="600"
-              style={{ textDecoration: "none" }}
-              href="https://mirror.xyz/0x55e2780588aa5000F464f700D2676fD0a22Ee160/8whNch3m5KMzeo6g5eblcXMMplPf8UpW228cSh3nmzg">
-              Learn more
-            </Typography>
-          </Box>
-          <Box component="p" margin="0">
-            <Typography
-              component="a"
-              color="#000"
-              fontWeight="600"
-              style={{ textDecoration: "none" }}
-              href="https://github.com/danfinlay/MobyMask/">
-              Fork on GitHub
-            </Typography>
-          </Box>
+    <Box backgroundColor="#F1F1F1" padding="90px 0">
+      <Box width="96%" maxWidth="960px" margin="auto">
+        <Box display="flex" justifyContent="flex-start" alignItems="center">
+          <Typography
+            component="img"
+            src={mobymaskLogo}
+            width="71px"
+            marginRight="15px"
+            alt="logo"
+          />
+          <Typography component="span" fontSize="31px" fontWeight="600">
+            MobyMask
+          </Typography>
         </Box>
 
-        <Box component="p" flexShrink="0" margin="0">
-          Mobymask is technically supported by Laconic and LXDAO
-        </Box>
-      </Box>
-      <Box
-        marginTop="43px"
-        display="flex"
-        justifyContent="flex-start"
-        flexWrap="wrap">
-        <Box
-          component="p"
-          width="456px"
-          color="#666F85"
-          display="flex"
-          justifyContent="flex-start"
-          flexShrink="0">
-          @2022 MobyMask • A ConsenSys Formation
-        </Box>
-        <Box display="flex" justifyContent="flex-start" flexShrink="0">
-          <Box component="p" marginRight="30px">
-            <Typography href="https://lxdao.io/">
+        <Box marginTop="60px" display="flex">
+          <Box flex="1">
+            <Box>
               <Typography
-                component="img"
-                src={lxdaoLogo}
-                height="40px"
-                alt="lxdao"
-              />
+                component="a"
+                color="#000"
+                fontWeight="600"
+                marginRight="30px"
+                target="_blank"
+                style={{ textDecoration: "none" }}
+                href="https://mirror.xyz/0x55e2780588aa5000F464f700D2676fD0a22Ee160/8whNch3m5KMzeo6g5eblcXMMplPf8UpW228cSh3nmzg"
+              >
+                Learn more
+              </Typography>
+
+              <Typography
+                component="a"
+                color="#000"
+                fontWeight="600"
+                target="_blank"
+                style={{ textDecoration: "none" }}
+                href="https://github.com/danfinlay/MobyMask/"
+              >
+                Fork on GitHub
+              </Typography>
+            </Box>
+            <Typography marginTop={5.5} color="#666F85">
+              @2022 MobyMask • MetaMask Ecosystem
             </Typography>
           </Box>
-          <Box component="p" color="#101828">
-            <Typography href="https://www.laconic.com/">
-              <Typography
-                component="img"
-                src={LaconicNetworklogo}
-                height="40px"
-                alt="laconic"
-              />
+          <Box flex="1">
+            <Typography fontWeight="bold">
+              MobyMask is technically supported by Laconic and LXDAO
             </Typography>
+
+            <Box marginTop={4} display="flex">
+              <Box marginRight="40px">
+                <Typography
+                  component="a"
+                  target="_blank"
+                  style={{ textDecoration: "none" }}
+                  href="https://lxdao.io/"
+                >
+                  <Box
+                    component="img"
+                    src={lxdaoLogo}
+                    height="40px"
+                    alt="lxdao"
+                  />
+                </Typography>
+              </Box>
+              <Box color="#101828">
+                <Typography
+                  component="a"
+                  target="_blank"
+                  style={{ textDecoration: "none" }}
+                  href="https://www.laconic.com/"
+                >
+                  <Box
+                    component="img"
+                    src={LaconicNetworklogo}
+                    height="40px"
+                    alt="laconic"
+                  />
+                </Typography>
+              </Box>
+            </Box>
           </Box>
         </Box>
       </Box>
