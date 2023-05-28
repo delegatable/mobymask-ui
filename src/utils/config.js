b6271355b7585427355b10d9bf7eea82f88c7d00
wwei-github <418047394@qq.com>
2023-05-28 20:56:06 +0800

    feat: change twitter link and github link

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,5 @@
 const hostname = window.location.hostname;
 
-console.log(hostname);
-
 const config = {
   "mobymask-ui.vercel.app": {
     address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
@@ -13,7 +11,7 @@ const config = {
     name: "MobyMask",
     chainId: 41337,
   },
-  mobymask: {
+  "mobymask.com": {
     address: "0xB06E6DB9288324738f04fCAAc910f5A60102C1F8",
     name: "MobyMask",
     chainId: 1,
